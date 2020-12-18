4fb7beaf4f90fa9f75e96381853229f70564b818
dangnhon <dang.nhon12345@yahoo.com>
2020-12-18 10:23:20 -0800

    finally

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+    belongs_to :author
+    validates :title, presence: true
+    validate :clickbaity
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"], message: "Not Valid" }
+    
+# This custom validation method was a pain in *ss
+    def clickbaity
+        array = ["Won't Believe", "Secret" , "Top[number]", "Guess"]
+        if self.title 
+            unless array.any? {|word| self.title.include? word}
+                errors.add(:title, "title is not clickbaity")
+            end 
+        end 
+    end
+
 end
